d2757a25f18e32abec76b54d70216f696523fd4b
Iván Domonkos <144695310+IvnDmnks@users.noreply.github.com>
2026-08-16 14:22:11 +0200

    myphotos page made

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,22 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ivandomonkos.site',
+        port: '',
+        pathname: '/api/media/file/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/media/file/**',
+      }
+    ]
+  }
 };
 
 export default withPayload(nextConfig);
